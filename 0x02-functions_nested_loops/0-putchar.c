c7b0e5c282c796e8a5afee3d3b65b94235185f14
RayoAdeoti <adeotiaderayo5@gmail>
2022-09-13 07:38:08 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,22 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- *  * main - prints Putchar
- *   *
- *    * Description: prints _putchar
- *     *
- *      * Return: 0 (Success)
+ * main - prints Putchar
+ *   
+ * Description: prints _putchar
+ *  
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-		_putchar('_');
-			_putchar('p');
-				_putchar('u');
-					_putchar('t');
-						_putchar('c');
-							_putchar('h');
-								_putchar('a');
-									_putchar('r');
-										_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');					       _putchar('a');					      _putchar('r');			
+	_putchar('\n');
 
-											return (0);
+							       return (0);
 }
